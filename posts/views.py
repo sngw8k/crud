4a037fd43e3581bb5c504247d2d26610a813d637
sngw8k <jso0304@naver.com>
2023-08-22 19:04:54 +0900

    Update 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -44,3 +44,29 @@ def delete(request, id):
 
     # 삭제 후 메인 페이지로 이동
     return redirect('/index/')
+
+def edit(request, id):
+    # 수정하기 위해 기존 정보 가지고 오기
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    # 기존 데이터
+    post = Post.objects.get(id=id)
+
+    # 방금 수정한 데이터
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # post = Post()  => 새로운 게시물 만들 떄
+    # 기존데이터에 덮어씌우기
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect(f'/posts/{post.id}/')
